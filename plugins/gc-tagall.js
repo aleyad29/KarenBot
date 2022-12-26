546e53039471a09b5381ed890fbd2abc27078faf
aleizn20 <virtualn641@gmail.com>
2022-12-26 16:37:48 -0500

    Bot creador por Wa.me/51914865141
    Bot creador por Wa.me/51914865141

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,10 +5,11 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+let teks = `Este bot fue creado por Wa.me/51914865141 No permito reventa
+*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
 teks += `┣➥ @${mem.id.split('@')[0]}\n`}
-teks += `*└* 𝐁𝐲 𝐍𝐨𝐭𝐉𝐡𝐮𝐳ᙆ  ᷦ\n\n*▌│█║▌║▌║║▌║▌║▌║█*`
+teks += `└𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 𝐀𝐥𝐞`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
